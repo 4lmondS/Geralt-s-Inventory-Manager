8d57a9a98fac45d45ed961a92b2b3720dc58488d
4lmondS <52657279+4lmondS@users.noreply.github.com>
2026-09-17 02:22:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,12 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     base: '/Geralt-s-Inventory-Manager/',
-
     plugins: [react(), tailwindcss()],
-
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
-
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
