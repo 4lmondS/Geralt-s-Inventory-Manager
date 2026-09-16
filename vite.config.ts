9c8d3cc7466859eab133a833d69e9f287d627e7a
4lmondS <52657279+4lmondS@users.noreply.github.com>
2026-09-17 02:10:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  base: '/Geralt-s-Inventory-Manager/',
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
